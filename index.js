7b6ce6fe04a7b44f4cf602bc1a4634cdd391c5a1
Sajid Noor Muhammad <153657286+SajidNoorMuhammad@users.noreply.github.com>
2024-12-14 13:59:30 +0500

    add index file

---
+++ index.js
@@ -0,0 +1,52 @@
+import express from "express"
+import morgan from "morgan";
+
+const tasks = [
+    {
+        id: 1,
+        task: "Drink Water"
+    },
+    {
+        id: 2,
+        task: "Complete Work"
+    },
+    {
+        id: 3,
+        task: "Playing Games"
+    }
+]
+
+
+const app = express();
+const PORT = 4000;
+
+app.use(morgan('tiny'))
+
+function middleware(req, res, next) {
+    req.requestBy = "Sajid Noor Muhammad";
+    next();
+}
+
+app.use(middleware)
+
+
+app.get('/', (req, res) => {
+    console.log("req.requestBy", req.requestBy)
+    res.status(200).send(tasks);
+})
+
+app.post('/', (req, res) => {
+    res.send('Post Request Called')
+})
+
+app.put('/', (req, res) => {
+    res.send('Put Request Called')
+})
+
+app.delete('/', (req, res) => {
+    res.send('Delete Request Called')
+})
+
+app.listen(PORT, () => {
+    console.log("Server is running on port" + PORT)
+})
