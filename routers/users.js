985c695bc9a545cdb0f1a3653b02322559768ad3
Sajid Noor Muhammad <153657286+SajidNoorMuhammad@users.noreply.github.com>
2024-12-16 00:47:47 +0500

    create user route.

--- routers/users.js
+++ routers/users.js
@@ -5,13 +5,24 @@ const router = express.Router();
 const users = [
     {
         fullName: "Sajid",
-        email: "sajid@gmail.com"
+        email: "sajid@gmail.com",
+        id: 1
     }
 ]
 
 router.get("/", (req, res) => {
+    res.status(200).json({
+        error: false,
+        data: users,
+        msg: "User Fetched Successfully"
+    })
+})
+
+router.post("/", (req, res) => {
     const { fullName, email } = req.body;
     users.push({ fullName, email, id: users.length + 1 });
+    console.log("fullname", fullName);
+    console.log("email", email)
     res.status(201).json({
         error: false,
         data: users,
@@ -20,17 +31,21 @@ router.get("/", (req, res) => {
 })
 
 router.get("/:id", (req, res) => {
-    const user = users.find((data) => data.id == req.params.id)
+    const user = users.find((data) => data.id == req.params.id);
 
-    if (!user) return
-    res.status(404).json({
-        error: true,
-        data: null,
-        msg: "User Not Found"
-    })
+    if (!user) {
+        return res.status(404).json({
+            error: true,
+            data: null,
+            msg: "User Not Found"
+        });
+    }
+
     res.status(200).json({
         error: false,
         data: user,
         msg: "User Found Successfully"
-    })
+    });
 })
+
+export default router
