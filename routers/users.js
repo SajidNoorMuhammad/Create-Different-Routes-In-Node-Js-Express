814d3b0414d415d542c81b470fc16a79f7950571
Sajid Noor Muhammad <153657286+SajidNoorMuhammad@users.noreply.github.com>
2024-12-15 23:28:26 +0500

    create user route.

--- routers/users.js
+++ routers/users.js
@@ -18,3 +18,19 @@ router.get("/", (req, res) => {
         msg: "User Added Successfully"
     })
 })
+
+router.get("/:id", (req, res) => {
+    const user = users.find((data) => data.id == req.params.id)
+
+    if (!user) return
+    res.status(404).json({
+        error: true,
+        data: null,
+        msg: "User Not Found"
+    })
+    res.status(200).json({
+        error: false,
+        data: user,
+        msg: "User Found Successfully"
+    })
+})
